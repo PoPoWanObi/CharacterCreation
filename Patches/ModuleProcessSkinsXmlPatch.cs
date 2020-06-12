dfce813f9ac63935757ced82451906ad17c78f11
Designer225 <designer2553@gmail.com>
2020-06-12 01:36:03 -0700

    Patch skins.xml loader
    The game combines skins.xml documents and seems to use the first `skins` array element (or the first matching `skin` nodes; implementation unclear) in the combined document for FaceGen. This means the extra sliders only show up if they are in a mod that is loaded before Native.
    
    Fixing this involves patching the skins.xml loader to reverse the file loading order, so modules loaded later will load their versions of skins.xml first. This will undoubtably break existing skins.xml mods, but the alternative is to rename DCC so that it doesn't break MCM, and the latter looks outright ugly by comparison.
    
    An alternative would be instead to modify the code so that Native's skins.xml is loaded last. This would maintain (a degree of) compatibility with skins.xml mods while guaranteeing that Native won't override modded skins.xml files.
    
    Also fixed post-build directives because a folder is only a folder if it ends with a back slash.

---
+++ Patches/ModuleProcessSkinsXmlPatch.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using HarmonyLib;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.ObjectSystem;
+
+namespace CharacterCreation.Patches
+{
+    [HarmonyPatch(typeof(Module), "CreateProcessedSkinsXMLForNative")]
+    public static class ModuleProcessSkinsXmlPatch
+    {
+        private static readonly TextObject DebugString =
+            new TextObject("{=CharacterCreation_ProcessSkinsXmlDebug}[CharacterCreation] Patching skins.xml loader to reverse file loading order");
+
+        [HarmonyPrefix]
+        public static bool Prefix(ref string __result, ref string baseSkinsXmlPath)
+        {
+            Debug.Print(DebugString.ToString());
+
+            // Why do I have to clone and modify two separate methods? - Designer225
+            List<string> elementNameList = new List<string>();
+            List<Tuple<string, string>> toBeMerged = new List<Tuple<string, string>>();
+            List<string> xsltList = new List<string>();
+
+            IEnumerable<MbObjectXmlInformation> mbprojXmlList = XmlResource.MbprojXmls.Where(x => x.Id == "soln_skins");
+            mbprojXmlList = mbprojXmlList.Reverse();
+
+            foreach (MbObjectXmlInformation mbprojXml in mbprojXmlList)
+            {
+                if (File.Exists(ModuleInfo.GetXmlPathForNative(mbprojXml.ModuleName, mbprojXml.Name)))
+                {
+                    elementNameList.Add(mbprojXml.Name);
+                    toBeMerged.Add(Tuple.Create(ModuleInfo.GetXmlPathForNative(mbprojXml.ModuleName, mbprojXml.Name), string.Empty));
+                }
+                string xsltPathForNative = ModuleInfo.GetXsltPathForNative(mbprojXml.ModuleName, mbprojXml.Name);
+                if (File.Exists(xsltPathForNative))
+                    xsltList.Add(xsltPathForNative);
+                else
+                    xsltList.Add("");
+            }
+            XmlDocument mergedXmlForNative = MBObjectManager.CreateMergedXmlFile(toBeMerged, xsltList, true);
+
+            System.IO.StringWriter stringWriter = new System.IO.StringWriter();
+            XmlTextWriter xmlTextWriter = new XmlTextWriter(stringWriter);
+            mergedXmlForNative.WriteTo(xmlTextWriter);
+            baseSkinsXmlPath = elementNameList.First();
+            __result = stringWriter.ToString();
+
+            return false;
+        }
+    }
+}
