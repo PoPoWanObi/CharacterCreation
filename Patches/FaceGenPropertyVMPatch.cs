e6e8acacd171c435af62531806c8b148fa445671
Designer225 <designer2553@gmail.com>
2020-10-07 17:20:51 -0700

    Create FaceGenPropertyVMPatch.cs
    Postfix patch to add the current value of a slider.

---
+++ Patches/FaceGenPropertyVMPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade.ViewModelCollection;
+
+namespace CharacterCreation.Patches
+{
+    [HarmonyPatch(typeof(FaceGenPropertyVM), nameof(FaceGenPropertyVM.Name), MethodType.Getter)]
+    static class FaceGenPropertyVMPatch
+    {
+        public static void Postfix(ref string __result, FaceGenPropertyVM __instance)
+        {
+            __result = $"{__result} {__instance.Value}";
+        }
+    }
+}
