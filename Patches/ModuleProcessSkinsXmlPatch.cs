93e20acd43da40a8df1015653efefb86c4e28670
Designer225 <designer2553@gmail.com>
2020-11-19 19:40:17 -0800

    Skin files updated + minor code change to native XML file loading

--- Patches/ModuleProcessSkinsXmlPatch.cs
+++ Patches/ModuleProcessSkinsXmlPatch.cs
@@ -23,30 +23,18 @@ namespace CharacterCreation.Patches
             Debug.Print(DebugString.ToString());
 
             // Why do I have to clone and modify two separate methods? - Designer225
-            List<string> elementNameList = new List<string>();
+            List<string> usedPaths = new List<string>();
             List<Tuple<string, string>> toBeMerged = new List<Tuple<string, string>>();
             List<string> xsltList = new List<string>();
 
             List<MbObjectXmlInformation> mbprojXmlList = XmlResource.MbprojXmls.Where(x => x.Id == "soln_skins").ToList();
             mbprojXmlList.Reverse(); // restored for compatibility reason with mods that modify skin.xml that load later.
 
-            //for (int i = 0; i < mbprojXmlList.Count; i++)
-            //{
-            //    var mbproj = mbprojXmlList[i];
-
-            //    if (mbproj.ModuleName == "Native")
-            //    {
-            //        mbprojXmlList.RemoveAt(i);
-            //        mbprojXmlList.Add(mbproj);
-            //        break;
-            //    }
-            //}
-
             foreach (MbObjectXmlInformation mbprojXml in mbprojXmlList)
             {
                 if (File.Exists(ModuleInfo.GetXmlPathForNative(mbprojXml.ModuleName, mbprojXml.Name)))
                 {
-                    elementNameList.Add(mbprojXml.Name);
+                    usedPaths.Add(ModuleInfo.GetXmlPathForNativeWBase(mbprojXml.ModuleName, mbprojXml.Name));
                     toBeMerged.Add(Tuple.Create(ModuleInfo.GetXmlPathForNative(mbprojXml.ModuleName, mbprojXml.Name), string.Empty));
                 }
                 string xsltPathForNative = ModuleInfo.GetXsltPathForNative(mbprojXml.ModuleName, mbprojXml.Name);
@@ -58,7 +46,7 @@ namespace CharacterCreation.Patches
             System.IO.StringWriter stringWriter = new System.IO.StringWriter();
             XmlTextWriter xmlTextWriter = new XmlTextWriter(stringWriter);
             mergedXmlForNative.WriteTo(xmlTextWriter);
-            baseSkinsXmlPath = elementNameList.First();
+            baseSkinsXmlPath = usedPaths.First();
             __result = stringWriter.ToString();
 
             return false;
