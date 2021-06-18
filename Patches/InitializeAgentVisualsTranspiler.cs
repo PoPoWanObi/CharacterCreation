bf859f9d9fc8bd61f8f33b22a1a0589f48fc0aa9
Designer225 <designer2553@gmail.com>
2021-06-18 02:32:31 -0700

    Create InitializeAgentVisualsTranspiler.cs
    Apparently if a character is less than 14 years old, the save preview considers them to be male. :derp:
    
    At least it's an easy fix. And it seems e1.6.0 might actually be an improvement overall.

---
+++ Patches/InitializeAgentVisualsTranspiler.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.View;
+
+namespace CharacterCreation.Patches
+{
+
+    [HarmonyPatch(typeof(BasicCharacterTableau), "InitializeAgentVisuals")]
+    static class InitializeAgentVisualsTranspiler
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var code = instructions.ToList();
+
+            // locate call to MBAgentVisuals function and work backwards
+            int i;
+            for (i = 0; i < code.Count; i++)
+            {
+                if (code[i].opcode == OpCodes.Call && code[i].operand is MethodInfo
+                    && code[i].operand as MethodInfo == AccessTools.Method(typeof(MBAgentVisuals), nameof(MBAgentVisuals.FillEntityWithBodyMeshesWithoutAgentVisuals)))
+                    break;
+            }
+                
+            if (i < code.Count)
+            {
+                for (; i >= 0; i--)
+                {
+                    if (code[i].opcode == OpCodes.Ldloc_2)
+                    {
+                        // replace current instruction
+                        code[i] = new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(BasicCharacterTableau), "_isFemale"));
+                        // insert ahead this
+                        code.Insert(i, new CodeInstruction(OpCodes.Ldarg_0)); // instance methods use ldarg_0 as this
+                        break;
+                    }
+                }
+            }
+
+            return code.AsEnumerable();
+        }
+    }
+}
