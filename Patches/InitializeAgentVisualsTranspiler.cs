cbf61be471abbba55e50a17bc00bd4de0dd8979a
Designer225 <designer2553@gmail.com>
2021-10-26 23:41:13 -0700

    Fixed error caused by TaleWorlds moving code

--- Patches/InitializeAgentVisualsTranspiler.cs
+++ Patches/InitializeAgentVisualsTranspiler.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using TaleWorlds.Core;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.MountAndBlade.View;
@@ -13,7 +14,7 @@ using TaleWorlds.MountAndBlade.View;
 namespace CharacterCreation.Patches
 {
 
-    [HarmonyPatch(typeof(BasicCharacterTableau), "InitializeAgentVisuals")]
+    [HarmonyPatch(typeof(BasicCharacterTableau), "RefreshCharacterTableau")]
     static class InitializeAgentVisualsTranspiler
     {
         private static readonly List<string> incompatibleInstances = new List<string>
@@ -25,37 +26,40 @@ namespace CharacterCreation.Patches
         {
             if (original == null) return true;
             var info = Harmony.GetPatchInfo(original);
-            if (info != default && info.Transpilers.Select(x => x.owner).Intersect(incompatibleInstances).Count() > 0)
+            if (info == default || info.Transpilers.Select(x => x.owner).Intersect(incompatibleInstances).IsEmpty())
             {
-                Debug.Print("[CharacterCreation] Patch to fix misgendered character rendering already exists, skipping");
-                return false;
+                Debug.Print("[CharacterCreation] Preparing to patch incorrect save preview stuff");
+                return true;
             }
-            return true;
+            Debug.Print("[CharacterCreation] Patch to fix misgendered character rendering already exists, skipping");
+            return false;
         }
 
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
+            Debug.Print("[CharacterCreation] Attempting to fix save preview misgendering");
             var code = instructions.ToList();
 
             // locate call to MBAgentVisuals function and work backwards
             int i;
             for (i = 0; i < code.Count; i++)
             {
-                if (code[i].opcode == OpCodes.Call && code[i].operand is MethodInfo
-                    && code[i].operand as MethodInfo == AccessTools.Method(typeof(MBAgentVisuals), nameof(MBAgentVisuals.FillEntityWithBodyMeshesWithoutAgentVisuals)))
+                if (code[i].opcode == OpCodes.Call && code[i].operand is MethodInfo method
+                    && method == AccessTools.Method(typeof(MBAgentVisuals), nameof(MBAgentVisuals.FillEntityWithBodyMeshesWithoutAgentVisuals)))
                     break;
             }
-                
+
             if (i < code.Count)
             {
                 for (; i >= 0; i--)
                 {
-                    if (code[i].opcode == OpCodes.Ldloc_2)
+                    if (code[i].opcode == OpCodes.Ldloc_S && code[i].operand is LocalBuilder lb && lb.LocalIndex == 5)
                     {
                         // replace current instruction
                         code[i] = new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(BasicCharacterTableau), "_isFemale"));
                         // insert ahead this
                         code.Insert(i, new CodeInstruction(OpCodes.Ldarg_0)); // instance methods use ldarg_0 as this
+                        Debug.Print("[CharacterCreation] Save preview misgendering fixed");
                         break;
                     }
                 }
