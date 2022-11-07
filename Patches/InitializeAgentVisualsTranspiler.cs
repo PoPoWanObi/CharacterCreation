adfa247892e6af26ae2946339e145ba6aedb8fa8
Designer225 <designer2553@gmail.com>
2022-11-06 17:13:15 -0800

    Update InitializeAgentVisualsTranspiler.cs
    Previous brain derp was not a derp at all and the correction turned out to be a derp. I was slow to realize local variable indices changed.

--- Patches/InitializeAgentVisualsTranspiler.cs
+++ Patches/InitializeAgentVisualsTranspiler.cs
@@ -7,6 +7,7 @@ using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using TaleWorlds.Core;
+using TaleWorlds.Engine;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.MountAndBlade.View;
@@ -54,7 +55,7 @@ namespace CharacterCreation.Patches
             {
                 for (; i >= 0; i--)
                 {
-                    if (code[i].opcode == OpCodes.Ldloc_S && code[i].operand is LocalBuilder lb && lb.LocalIndex == 5)
+                    if (code[i].opcode == OpCodes.Ldloc_S && code[i].operand is LocalBuilder lb && lb.LocalIndex == 4)
                     {
                         // replace current instruction
                         code[i] = new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(BasicCharacterTableau), "_isFemale"));
