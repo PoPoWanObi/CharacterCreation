74c0ed16e463e9bb51c12f1d9eabb082e781424d
Designer225 <designer225@protonmail.com>
2025-12-23 22:03:36 -0800

    Actually fix build to include conditional loader library
    - Also tried to fix possible bug with file loading (might be scuffed by default if it assumes a certain directory).
    - Conditional loader is also updated with another fix. I will publish it eventually.

--- CharacterCreation/CharacterCreationAssemblyLoader.cs
+++ CharacterCreation/CharacterCreationAssemblyLoader.cs
@@ -1,4 +1,7 @@
-﻿using ConditionalAssemblyLoader;
+﻿using System.IO;
+using ConditionalAssemblyLoader;
+using TaleWorlds.Library;
+using TaleWorlds.ModuleManager;
 
 namespace CharacterCreation
 {
@@ -6,7 +9,13 @@ namespace CharacterCreation
     {
         public CharacterCreationAssemblyLoader()
         {
-            References.Add(new ConditionalAssemblyReference(() => true, "CharacterCreation.1.7.0.dll"));
+
+            var binaryPath = Path.Combine(Path.GetFullPath(ModuleHelper.GetModuleFullPath("zzCharacterCreation")),
+                "bin", "Win64_Shipping_Client");
+            References.Add(new ConditionalAssemblyReference(() => true, "CharacterCreation.1.7.0",
+                Path.Combine(binaryPath, "CharacterCreation.1.7.0.dll")));
+            Out = str => Debug.Print(str);
+            Error = str => Debug.Print(str);
         }
         
         protected override void OnAssemblyLoaded(CharacterCreationEntryPoint value)
