068af70f6519e247a1e440ebe298112e10b1ea1b
Designer225 <designer225@protonmail.com>
2026-01-06 00:15:59 -0800

    Switch to Bannerlord SDK

--- CharacterCreation/SubModule.cs
+++ CharacterCreation/SubModule.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using BUTR.MessageBoxPInvoke.Helpers;
+using CharacterCreation.CampaignSystem;
+using CharacterCreation.Compatibility;
+using CharacterCreation.Models;
+using CharacterCreation.Settings;
+using CharacterCreation.UI;
 using CharacterCreation.Util;
+using HarmonyLib;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Encyclopedia;
 using TaleWorlds.Core;
 using TaleWorlds.Library;
 using TaleWorlds.Localization;
 using TaleWorlds.MountAndBlade;
 using static CharacterCreation.Util.DccLocalization;
+using FaceGen = TaleWorlds.Core.FaceGen;
 
 namespace CharacterCreation
 {
@@ -26,20 +34,13 @@ namespace CharacterCreation
         
         public static SubModule? Instance { get; private set; }
 
-        public readonly CommandImplementation CommandImplementation;
-
-        private readonly CharacterCreationEntryPoint _entryPoint;
         private bool _isLoaded;
-        
-        public CharacterCreationEntryPoint EntryPoint => _entryPoint;
+
+        private readonly Harmony _harmony;
 
         public SubModule()
         {
-            if (!new CharacterCreationAssemblyLoader().TryLoad(out var result, out var error))
-                throw error!;
-
-            _entryPoint = result.Instance;
-            CommandImplementation = _entryPoint.GetCommandImplementation();
+            _harmony = new Harmony("mod.bannerlord.popowanobi.dcc");
             Instance = this;
         }
 
@@ -52,7 +53,13 @@ namespace CharacterCreation
             base.OnSubModuleLoad();
             try
             {
-                _entryPoint.OnBeforeInitialModuleScreenSetAsRoot();
+                _harmony.PatchAll();
+
+                // apply compatibility patches
+                if (DccSettings.Instance!.EnableCompatibility)
+                    CompatibilityPatch.PatchAll(_harmony);
+
+                FaceGen.ShowDebugValues = true; // Developer facegen
             }
             catch (Exception ex)
             {
@@ -66,13 +73,44 @@ namespace CharacterCreation
         }
 
         // Called when loading save game
-        public override void OnGameLoaded(Game game, object initializerObject) =>
-            _entryPoint.OnGameLoaded(game, initializerObject);
+        public override void OnGameLoaded(Game game, object initializerObject)
+        {
+            if (!(game.GameType is Campaign) || !DccSettings.Instance!.DebugMode) return;
+
+            var player = Hero.MainHero;
+            if (player is null) return;
+            InformationManager.DisplayMessage(
+                new InformationMessage(GetFormattedAgeDebugMessage(player, player.Age),
+                    ColorManager.Red));
+            Debug.Print(GetFormattedAgeDebugMessage(player, player.Age));
+        }
 
         // called after game is initialized
-        public override void OnGameInitializationFinished(Game game) => _entryPoint.OnGameInitializationFinished(game);
+        public override void OnGameInitializationFinished(Game game)
+        {
+            // just to make sure facegen is set
+            FaceGen.ShowDebugValues = true;
+            // make sure to call this and other daily tick events on... well, daily tick
+            if (game.GameType is Campaign)
+                SettingsEffects.Initialize(true);
+        }
 
-        protected override void OnGameStart(Game game, IGameStarter gameStarterObject) =>
-            _entryPoint.OnGameStart(game, gameStarterObject);
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            if (!(game.GameType is Campaign) || !(gameStarterObject is CampaignGameStarter gameStarter))
+                return;
+
+            // add behaviors
+            gameStarter.AddBehavior(new CharacterCreationCampaignBehavior());
+
+            // add game models
+            if (DccSettings.Instance!.CustomAgeModel)
+                gameStarter.AddModel(new DccAgeModel());
+
+            // add event handlers
+            game.AddGameHandler<AgingGameHandler>();
+            game.EventManager.RegisterEvent<EncyclopediaPageChangedEvent>(new EncyclopediaPageChangedAction()
+                .OnEncyclopediaPageChanged);
+        }
     }
 }
